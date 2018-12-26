da3a1c3810ce890dfbe993215d6446ba1741c775
yauhenii khudzik <yauhenii.khudzik@gmail.com>
2018-12-26 13:19:33 +0300

    preloader added port configuration for dev server added

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.base.config');
 
@@ -5,6 +6,8 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
     }
 });
