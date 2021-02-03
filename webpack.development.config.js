52047febe0ea60fbf1067e33ab415dc90f579e5d
Eugene Khudik <eugene.khudik@gmail.com>
2021-02-03 17:15:08 +0300

    Deps versions upgrade

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge')
 const common = require('./webpack.base.config');
 
 module.exports = merge(common, {
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge')
 const common = require('./webpack.base.config');
 
 module.exports = merge(common, {
